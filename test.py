84c7cb128f8525dd8e32acc05fe4dbf3f3755d40
Esa Kesti <esakesti@gmail.com>
2024-03-02 23:22:00 +0000

    Flask learning

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+from packaging.module1 import square, doubler
+from packaging.module2 import mean
+
+print("4^2 = " ,square(4))
+print("2*4 = ", doubler(4))
+print("(2+1+3)/3 = ", mean([2,1,3]))
